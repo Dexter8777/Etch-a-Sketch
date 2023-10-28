14e1d7bf0b5b204fb1bc6df8ba4e63a0720ba2c9
Dexter8777 <mosesdexter2019@gmail.com>
2023-10-28 18:05:28 +0100

    Create a function that will create the 16 by 16 grid

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const gridContainer = document.getElementsByClassName('grid-container');
+const gridContainer = document.querySelector('.grid-container');
 
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cell');
+            gridContainer.appendChild(gridCell);
+        }
+    }
+}
+
+createGrid();
