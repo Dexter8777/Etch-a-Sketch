b51b9dc6fdd5bbbc793b4c9144bd8dfcf14188e6
Dexter8777 <mosesdexter2019@gmail.com>
2023-11-02 22:46:26 +0000

    Declare 'gridCell' with let instead of const
    Previously gridCell was defined with const, however this meant its value
    could not be reassigned, further meaning that gridCell had to declared
    in the for loop during every iterations(16 times).
    
    Now, declaring gridCell with 'let' allows it to be assigned a new div
    during each iteration outside the for loop as its value can change
    allowing its use elsewhere in the program.

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const gridContainer = document.querySelector('.grid-container');
+let gridCell;
 
 function createGrid() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
-            const gridCell = document.createElement('div');
+            gridCell = document.createElement('div');
             gridCell.classList.add('grid-cell');
             gridContainer.appendChild(gridCell);
-        }
-    }
+        } 
+    } 
+   
 }
 
-function addHoverEffect(){
-    
-}
+// function addHoverEffect(){
+//     gridContainer.addEventListener('mouseover', () => {
+//         gridContainer.classList.add('change-color');
+//     })
+// }
 
 createGrid();
+// addHoverEffect();
